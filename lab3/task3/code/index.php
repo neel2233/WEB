26a55fbc9b75d3e0ac5a1734094557ccb9674475
neel2233 <117990551+neel2233@users.noreply.github.com>
2024-03-30 17:14:16 +0200

    add task3.a.iii and task3.a.iv

--- lab3/task3/code/index.php
+++ lab3/task3/code/index.php
@@ -7,6 +7,7 @@
     <title>Доска Объявлений</title>
 </head>
 <body>
+<div id='form'>
     <form action='save.php' method='post'>
         <label for='email'>Email</label>
         <input type='email' name='email'></input><br>
@@ -16,8 +17,10 @@
             <?php
             $categories = [];
             $dir = opendir('categories');
-            while($file = readdir($dir)) {
-            if (is_dir('categories/'.$file) && $file != '.' && $file != '..') {
+            while($file = readdir($dir)) 
+            {
+            if (is_dir('categories/'.$file) && $file != '.' && $file != '..') 
+            {
                 array_push($categories, $file);
             }
             }
@@ -37,6 +40,72 @@
 
         <input type='submit' value='Добавить'></input>
         </form>
+</div>
 
+<div id='table'>
+        <table>
+            <thread>
+                <th>Категория</th>
+                <th>Название</th>
+                <th>Email</th>
+                <th>Текст объявления</th>
+            </thread>
+            <tbody>
+            <?php
+            $categories = [];
+            $dir = opendir('categories');
+            while($file = readdir($dir)) 
+            {
+            if (is_dir('categories/'.$file) && $file != '.' && $file != '..') 
+            {
+                array_push($categories, $file);
+            }
+            }
+            closedir($dir);
+            foreach($categories as $category)
+            {
+                $users = [];
+                $dir = opendir("categories/$category");
+                while($file = readdir($dir)) 
+                {
+                    if (is_dir("categories/$category/".$file) && $file != '.' && $file != '..') 
+                    {
+                        array_push($users, $file);
+                    }
+                }
+                closedir($dir);
+                foreach($users as $user)
+                {
+                    $items = [];
+                    $dir = opendir("categories/$category/$user");
+                    while($file = readdir($dir)) 
+                    {
+                        if (is_file("categories/$category/$user/".$file) && $file != '.' && $file != '..') 
+                        {
+                            array_push($items, $file);
+                        }
+                    }
+                    closedir($dir);
+                        foreach($items as $item)
+                        {
+                            $file = fopen("categories/$category/$user/$item", 'r');
+                            $text = fread($file, filesize("categories/$category/$user/$item"));
+                            fclose($file);
+                            $title = preg_replace('/.txt/', '', $item);
+                            echo "
+                            <tr>
+                                <td>$category</td>
+                                <td>$title</td>
+                                <td>$user</td>
+                                <td>$text</td>
+                            </tr>
+                            ";
+                    }
+                }
+            }
+            ?>
+            </tbody>
+        </table>
+</div>
 </body>
 </html>
--- lab3/task3/code/save.php
+++ lab3/task3/code/save.php
@@ -1,1 +1,30 @@
 <?php
+function redirectToHome()
+{
+    header('Location: index.php');
+    exit();
+}
+
+if(false === isset($_POST['email'], $_POST['category'], $_POST['title'], $_POST['text']))
+{
+    redirectToHome();
+}
+
+$category = $_POST['category'];
+$title = $_POST['title'];
+$text = $_POST['text'];
+$email = $_POST['email'];
+if(!is_dir("categories/$category/$email"))
+{
+    mkdir("categories/$category/$email");
+}
+chmod("categories/$category/$email", 0777);
+$filePath = "categories/$category/$email/$title.txt";
+
+
+if(false === file_put_contents($filePath, $text))
+{
+    throw new Exception('Somathing went wrong');
+}
+chmod($filePath, 0777);
+redirectToHome();
