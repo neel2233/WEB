b0f056da0851a3407001e8568528f1ef2b9d250c
neel2233 <117990551+neel2233@users.noreply.github.com>
2024-03-30 12:28:43 +0200

    add task1.b

--- lab3/task1/code/index.php
+++ lab3/task1/code/index.php
@@ -12,4 +12,19 @@
  $str = 'ahb acb aeb aeeb adcb axeb';
 var_dump(findSubStr($str));
 
+// b
+function cube($match)
+{
+    return $match[0]** 3;
+}
 
+function replaceNums($str)
+{
+    
+    $regexp = '/[0-9]+/';
+    return preg_replace_callback($regexp, 'cube', $str);
+}
+
+$str = 'a1b2c3';
+echo replaceNums($str);
+echo "\n";
